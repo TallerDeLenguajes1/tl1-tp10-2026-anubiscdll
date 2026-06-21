b0eecf3b95c30489c51871a4fb3649ab03914652
anubiscdll <anubisbalbo@gmail.com>
2026-06-21 20:27:06 -0300

    Ejercicio 1 terminado

--- Tareas/Program.cs
+++ Tareas/Program.cs
@@ -1,2 +1,4 @@
 ﻿using EspacioTareas;
-Tarea.ObtenerTareas().Wait();
+List<Tarea> Tareas = await Tarea.ObtenerTareas();
+bool guardado = await Tarea.GuardarTareasEnArchivo(Tareas);
+Console.WriteLine($"Tareas guardadas: {guardado}");
--- Tareas/Tareas.cs
+++ Tareas/Tareas.cs
@@ -3,7 +3,7 @@ namespace EspacioTareas
     using System;
     using System.Text.Json;
     using System.Text.Json.Serialization;
-
+    using System.IO;
     public class Tarea
     {
         [JsonPropertyName("userId")]
@@ -17,20 +17,56 @@ namespace EspacioTareas
 
         private static readonly HttpClient httpClient = new HttpClient();
 
-        public static async Task ObtenerTareas()
+        public static async Task<List<Tarea>> ObtenerTareas()
         {
-            await ObtenerTareasAsync();
+            return await ObtenerTareasAsync();
         }
-        private static async Task ObtenerTareasAsync()
+        private static async Task<List<Tarea>> ObtenerTareasAsync()
         {
             var url = "https://jsonplaceholder.typicode.com/todos";
             HttpResponseMessage response = await httpClient.GetAsync(url);
             response.EnsureSuccessStatusCode();
             string responseBody = await response.Content.ReadAsStringAsync();
             List<Tarea> tareas = JsonSerializer.Deserialize<List<Tarea>>(responseBody);
+            Console.WriteLine("----------------Tareas Pendientes----------------");
             foreach (var tarea in tareas)
             {
-                Console.WriteLine($"ID: {tarea.Id}, Title: {tarea.Title}, Completed: {tarea.Completed}");
+                if (!tarea.Completed)
+                {
+                    Console.WriteLine($"Title: {tarea.Title}, Completed: {tarea.Completed}");
+                }
+            }
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("");
+            Console.WriteLine("----------------Tareas Completadas");
+            foreach (var tarea in tareas)
+            {
+                if (tarea.Completed)
+                {
+                    Console.WriteLine($"Title: {tarea.Title}, Completed: {tarea.Completed}");
+                }
+            }
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("");
+            return tareas;
+        }
+        public static async Task<bool> GuardarTareasEnArchivo(List<Tarea> tareas)
+        {
+            try
+            {
+                string jsonString = JsonSerializer.Serialize(tareas);
+                string filePath = Directory.GetCurrentDirectory() + "/tareas.json";
+                Console.WriteLine($"Guardando tareas en el archivo: {filePath}");
+                File.WriteAllText(filePath, jsonString);
+                Console.WriteLine($"Tareas guardadas en el archivo: {filePath}");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al guardar las tareas en el archivo: {ex.Message}");
+                return false;
             }
         }
 
